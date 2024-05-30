47d9839978212062bf267c50dc23c3159601db48
Mans <m.ramazanov@wi-fi.click>
2024-05-31 00:44:36 +0300

    add imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '/home/mans/Рабочий стол/test/Data_heroes_test/data_heroes_test/src/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
